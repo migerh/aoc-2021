037f73e9ffe48e1436a49726ab9a4967411b1b97
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2021-12-06 07:00:19 +0100

    Improve solution for day 6

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,76 +1,58 @@
 use std::num::ParseIntError;
 use crate::utils::ParseError;
 
-#[derive(Debug, Clone)]
-pub struct Fish {
-    days: usize,
-}
-
-impl Fish {
-    pub fn new(days: usize) -> Self {
-        Fish { days }
-    }
-
-    pub fn tick(&mut self) -> bool {
-        if self.days == 0 {
-            self.days = 6;
-            return true;
-        }
-
-        self.days -= 1;
-        false
-    }
-}
-
 #[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> Result<Vec<Fish>, ParseIntError> {
-    Ok(input
+pub fn input_generator(input: &str) -> Result<Vec<u8>, ParseIntError> {
+    input
         .split(",")
         .filter(|s| *s != "")
-        .map(|s| s.parse::<usize>())
-        .collect::<Result<Vec<_>, ParseIntError>>()?
-        .iter()
-        .map(|f| Fish::new(*f))
-        .collect())
+        .map(|s| s.parse::<u8>())
+        .collect::<Result<Vec<_>, ParseIntError>>()
+}
+
+type Buckets = [usize; 9];
+
+fn breed(buckets: &mut Buckets) {
+    // This is basically a rotate_left but for better readability
+    // with regards to accounting for the new born fish, we will
+    // write it down explicitly.
+    let breeding = buckets[0];
+    for i in 0..8 {
+        buckets[i] = buckets[i + 1];
+    }
+    buckets[6] += breeding;
+    buckets[8] = breeding;
+}
+
+fn school_to_buckets(school: &Vec<u8>) -> Buckets {
+    let mut bucket = [0; 9];
+
+    for f in school {
+        bucket[*f as usize] += 1;
+    }
+
+    bucket
 }
 
 #[aoc(day6, part1)]
-pub fn solve_part1(input: &Vec<Fish>) -> Result<usize, ParseError> {
-    let mut fish = input.clone();
-    let days = 80;
-    let mut next;
+pub fn solve_part1(input: &Vec<u8>) -> Result<usize, ParseError> {
+    let mut bucket = school_to_buckets(input);
 
+    let days = 80;
     for _ in 0..days {
-        next = vec![];
-        for mut f in fish.into_iter() {
-            if f.tick() {
-                next.push(Fish::new(8))
-            }
-            next.push(f);
-        }
-        fish = next;
+        breed(&mut bucket)
     }
 
-    Ok(fish.len())
+    Ok(bucket.iter().sum())
 }
 
 #[aoc(day6, part2)]
-pub fn solve_part2(input: &Vec<Fish>) -> Result<usize, ParseError> {
-    let mut bucket = vec![0; 9];
-
-    for f in input {
-        bucket[f.days] += 1;
-    }
+pub fn solve_part2(input: &Vec<u8>) -> Result<usize, ParseError> {
+    let mut bucket = school_to_buckets(input);
 
     let days = 256;
-
     for _ in 0..days {
-        let temp = bucket[0];
-        for i in 0..8 {
-            bucket[i] = bucket[i + 1];
-        }
-        bucket[6] += temp;
-        bucket[8] = temp;
+        breed(&mut bucket);
     }
 
     Ok(bucket.iter().sum())
@@ -85,7 +67,7 @@ mod test {
         "3,4,3,1,2"
     }
 
-    fn input() -> Result<Vec<Fish>, ParseError> {
+    fn input() -> Result<Vec<u8>, ParseError> {
         Ok(input_generator(sample())?)
     }
 
